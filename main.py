e4e8ba86aba5dd1b92a52816dd50af710f1c4c96
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2024-08-09 12:56:53 -0300

    Trabalhando no reconhecimento de mão

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import cv2 as cv
-import numpy as np
 import threading
 import logging
+
+import cv2 as cv
+import numpy as np
 import pyaudio
+import mediapipe as mp
+import tensorflow as tf
+#from tf.keras.models import load_model
 
 # https://techvidvan.com/tutorials/hand-gesture-recognition-tensorflow-opencv/
 # https://realpython.com/traditional-face-detection-python/
@@ -10,17 +14,21 @@ import pyaudio
 
 class Teremin:
     def __init__(self):
+        # INFORMAÇÕES
         print(f"TEREMIN VIRTUAL")
         print(f"por Ewerton Bramos")
         print(f"(pressione \"q\" para sair)")
 
+        # INICIALIZANDO LOGGING
         format = "%(asctime)s: %(message)s"
         logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
+        # ADICIONANDO HAAR-LIKE CLASSIFIER
         classifierXML = 'haarcascade_frontalface_alt.xml'
         self.faceCascade:cv.CascadeClassifier = cv.CascadeClassifier(classifierXML)
         #faceCascade = cv.CascadeClassifier('haarcascade_smile.xml')
 
+        # INICIALIZANDO PYAUDIO
         self.pyAudio:pyaudio.PyAudio = pyaudio.PyAudio()
         self.volume:float = 0.5  # range [0.0, 1.0]
         self.fs:int = 44100  # sampling rate, Hz, must be integer
@@ -35,6 +43,22 @@ class Teremin:
                         output=True)
 
         self.capturaVideo:cv.VideoCapture = cv.VideoCapture(0)
+
+        # INICIALIZANDO HAND GESTURE
+        mpHands = mp.solutions.hands
+        hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+        mpDraw = mp.solutions.drawing_utils
+
+        # Load the gesture recognizer model
+        '''model = tf.keras.models.load_model('hand_gesture_recognition_code/mp_hand_gesture')
+        #model = tf.keras.layers.TFSMLayer(
+        #    'hand-gesture-recognition-code/mp_hand_gesture/',
+        #    )#call_endpoint='serving_default')
+        # Load class names
+        f = open('gesture.names', 'r')
+        classNames = f.read().split('\n')
+        f.close()
+        print(classNames)'''
 
         self.sair:bool = False
         self.main()
@@ -54,13 +78,46 @@ class Teremin:
         freq = self.startFreq + proximidade
         sample = self.gerarSom(freq, self.duracao)
         logging.info(f"Proximidade: {proximidade} | {freq}Hz")
-        self.stream.write(sample)
+        
+        if self.stream.get_write_available():
+            self.stream.write(sample)
+
+    '''def reconhecerMao(self, frame, hands, mpHands, mpDraw, classNames, model):
+        framergb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        # Get hand landmark prediction
+        result = hands.process(framergb)
+        className = ''
+        # post process the result
+        if result.multi_hand_landmarks:
+                landmarks = []
+                for handslms in result.multi_hand_landmarks:
+                    for lm in handslms.landmark:
+                        # print(id, lm)
+                        lmx = int(lm.x * x)
+                        lmy = int(lm.y * y)
+                        landmarks.append([lmx, lmy])
+                    # Drawing landmarks on frames
+                    mpDraw.draw_landmarks(frame, handslms, 
+        mpHands.HAND_CONNECTIONS)
+                    
+        # RECONHECER GESTOS
+        prediction = model.predict([landmarks])
+        print(prediction)
+        classID = np.argmax(prediction)
+        className = classNames[classID]
+        # show the prediction on the frame
+        cv.putText(frame, className, (10, 50), cv.FONT_HERSHEY_SIMPLEX,
+                        1, (0,0,255), 2, cv.LINE_AA)'''
 
     def main(self):
         while self.sair == False:
+            # Capturando frame do vídeo da webcam e espelhando
+            ret, frame = self.capturaVideo.read()
+            #frameX, frameY, frameC = frame.shape
+            #frame = cv.flip(frame,1)
+
             # Carregando imagem em escala de cinza para detecção de Haar-Like
             # feature usando o algoritmo de Viola-Jones
-            ret, frame = self.capturaVideo.read()
             gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
             faces = self.faceCascade.detectMultiScale(
@@ -83,8 +140,9 @@ class Teremin:
                 cv.rectangle(frame, (x, y), (x+w, y+h), rectCor, 2)
 
                 normalizado = max(0,min(w - 100,self.quantSamples - 1))
-                t = threading.Thread(target=self.tocarSom, args=(normalizado,))
-                t.start()
+                self.tocarSom(normalizado)
+                #t = threading.Thread(target=self.tocarSom, args=(normalizado,))
+                #t.start()
 
                 cv.putText(frame,
                            f"{self.startFreq + normalizado}Hz",
